da9c13221400ccb015e1c33453ff4e8382950ad0
feoalexei <feoalexei@ukr.net>
2020-05-21 22:41:28 +0300

    fixed methods

--- src/index.js
+++ src/index.js
@@ -23,20 +23,23 @@ class Student extends User {
 
         this.marksArr = new Array(30);
         this.visitArr = new Array(30);
+    }
 
-        const getIndex = (array) => array.findIndex(element => element === undefined)
+    mark(ball) {
+        const markIndex = this.marksArr.findIndex(element => element === undefined);
+        this.marksArr[markIndex] = ball;
+    } 
 
-        this.mark = ball => this.marksArr[getIndex(this.marksArr)] = ball;
-
-        this.visiting = visit => {
-            visit ? this.visitArr[getIndex(this.visitArr)] = '+'
-                : this.visitArr[getIndex(this.visitArr)] = '-';
-        }
+    visiting(visit) { // visiting argument may be 'true' or 'false'
+        const visitIndex = this.visitArr.findIndex(element => element === undefined);
+        if (visit) {
+            this.visitArr[visitIndex] = true;
+        } else {this.visitArr[visitIndex] = false;}
     }
 
     get averageVisit() {
         const lessons = this.visitArr.filter(lesson => lesson !== undefined);
-        const trueVisits = this.visitArr.filter(visit => visit === '+');
+        const trueVisits = this.visitArr.filter(visit => visit === true);
         return trueVisits.length / lessons.length;
     }
 
@@ -53,7 +56,7 @@ class Teacher extends User {
         this.groups = [];
     }
 
-    set addGroup(groupName) {
+    addGroup(groupName) {
         this.groups.push({
             name: groupName,
             isActive: true
@@ -83,19 +86,27 @@ class Teacher extends User {
 // const winston = new Student('Winston', 'Smith', 1954);
 // const george = new Teacher('George', 'Orwell', 1984);
 
-// george.addGroup = 'Front-End Pro';
-// george.addGroup = 'Front-End Basic';
-// george.addGroup = 'DevOps';
+// george.addGroup('Front-End Pro');
+// george.addGroup('Front-End Basic');
+// george.addGroup('DevOps');
+// george.addGroup('PHP');
 
 // george.changeStatus('Front-End Pro', false);
 // george.changeStatus('Front-End Pro');
 // george.changeStatus('DevOps', false);
 
+
 // winston.mark(5);
 // winston.mark(10);
 
 // winston.visiting(true);
-// winston.visiting(false);ls
+// winston.visiting(false);
+// winston.visiting(false);
+
+// console.log(winston);
+// console.log(winston.visitArr);
+// console.log(winston.averageVisit);
+// console.log(winston.averageMark);
 
 // console.log(george);
 // console.log(george.groups);
